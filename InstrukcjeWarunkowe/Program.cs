5bc824b7587d0731fa7f7df9ac2c942d8a98f88e
nemmip <julkazbs2000@gmail.com>
2024-01-11 17:35:54 +0100

    Instrukcje Warunkowe i Zmienne teoria + zmieniony gitignore

---
+++ InstrukcjeWarunkowe/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // if (wyrazenie logiczne)
+            // polecenie; --> jednolinijkowe
+
+            // if (wyrazenie logiczne) 
+            // {
+            // blok kodu;
+            // }
+            //int a = 5, b=3;
+
+            //if (a < b)
+            //{
+            //    Console.WriteLine("A jest mniejsze od B");
+            //}
+            //Console.ReadKey();
+
+            // if (wyrazenie logiczne) 
+            // {
+            // blok kodu
+            // }
+            //else 
+            // {
+            // blok kodu
+            // }
+
+            //int a = 3, b = 3;
+            //// < > <= >=
+            //if (a <= b)
+            //{
+            //    Console.WriteLine("A jest mniejsze od B");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("B jest mniejsze od A");
+            //}
+
+
+            //if (a < b)
+            //{
+            //    Console.WriteLine("A jest mniejsze od B");
+            //}
+            //Console.WriteLine("B jest mniejsze od A");
+
+            //int a = 1;
+
+            //if (a == 1 || a == -1)
+            //{
+            //    Console.WriteLine("Podana wartosc jest wartoscia bezwzgledna liczby 1");
+            //}
+
+            //int x=2, y=3, z = 5;
+
+            //if(x*y == 6 && 10 % z == 0 && z==5) 
+            //{
+            //    Console.WriteLine("Super");
+            //}
+
+            //string imie, nazwisko;
+            //Console.WriteLine("Podaj imie: ");
+            //imie = Console.ReadLine();
+            //Console.WriteLine("Podaj nazwisko: ");
+            //nazwisko = Console.ReadLine();
+            //Console.WriteLine(" ".Length);
+            //if(imie.Length + 1 + nazwisko.Length > 30)
+            //{
+            //    Console.WriteLine("Za dlugie imie i nazwisko");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Wszystko ok");
+            //}
+
+            //Console.ReadKey();
+
+            //double przychod = 1000.00, koszty = 900.00;
+
+            //double dochod = przychod - koszty;
+            //if (dochod == 0)
+            //{
+            //    Console.WriteLine("Dochod jest zerowy");
+            //}
+            //if (dochod > 0)
+            //{
+            //    Console.WriteLine("Mamy zysk");
+            //}
+            //if (dochod < 0)
+            //{
+            //    Console.WriteLine("Mamy strate");
+            //}
+
+            //if (dochod == 0)
+            //{
+            //    Console.WriteLine("Dochod jest zerowy");
+            //}
+            //else
+            //{
+            //    if (dochod > 0)
+            //    {
+            //        Console.WriteLine("Mamy zysk");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Mamy strate");
+            //    }
+            //}
+
+            //if (dochod != 0)
+            //{
+            //    if (dochod > 0)
+            //    {
+            //        Console.WriteLine("Mamy zysk");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Mamy strate");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Dochod jest zerowy");
+            //}
+
+            int dzien = int.Parse(Console.ReadLine());
+
+            //if (dzien == 1)
+            //{
+            //    Console.WriteLine("Poniedzialek");
+            //}
+            //else if (dzien == 2)
+            //{
+            //    Console.WriteLine("Wtorek");
+            //}
+            //else if(dzien == 3)
+            //{
+            //    Console.WriteLine("Sroda");
+
+            //}
+            //else if (dzien == 4) 
+            //{
+            //    Console.WriteLine("Czwartek");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Nie ma takiego dnia");
+            //}
+
+            switch (dzien)
+            {
+                case 1:
+                    Console.WriteLine("Poniedzialek");
+                    break;
+                case 2:
+                    Console.WriteLine("Wtorek");
+                    break;
+                case 3:
+                    Console.WriteLine("Sroda");
+                    break;
+                case 4:
+                    Console.WriteLine("Czwartek");
+                    break;
+                default: 
+                    Console.WriteLine("Nie ma takiego dnia");
+                    break;
+            }
+
+            bool prawda = 1 > 0;
+            int niewiadomoCo = prawda ? 1 : 0;
+
+            Console.WriteLine(niewiadomoCo);
+        }
+    }
+}
---
+++ Zmienne/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!"); // wyswietla hello world
+            //Console.ReadKey(); // czyta klawisz
+            //// komentarz
+            ///*
+            // * to jest dlugi komentarz
+            // */
+            //// deklaracjwe zmiennych
+            //// C# => [const] TYP NAZWA [= WARTOSC];
+            //// Python => NAZWA [: TYP] [= WARTOSC]
+
+            //bool prawda;
+            //bool falsz = false;
+            //Console.WriteLine(falsz);
+            //Console.ReadKey();
+            //// int , float , double , decimal, string , bool 
+
+            //int x = 5;
+            //int y = x;
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            //x = 6;
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+
+            //int a = 1, b, c = 5;
+            //Console.WriteLine(a);
+
+            ////matematyka
+            //int a = 4, b = 1, wynik;
+            //wynik = 2 * a - 3 * b; // 2a - 3b
+            //Console.WriteLine(wynik);
+            //Console.ReadLine();
+            //double wynik1 = (double)b / a; // rzutowanie
+            //Console.WriteLine(wynik1);
+            //double testZmienne = 4d; // d lub D
+            //float testFloat = 3.5F; // f F
+            //decimal testDec = 2.3m; // m M
+            //long testLong = 123l; // l L
+
+            //Console.WriteLine("Podaj liczby wieksze od 0");
+            //double x, y;
+            //x = Convert.ToDouble(Console.ReadLine());
+            //y = Math.Sqrt(Math.Abs(Math.Sin(x)) * Math.Log(x, 2.0));
+            //Console.WriteLine(y);
+            //Console.ReadLine();
+
+            //// ++ --
+            //int x = 0, y;
+            //y = ++x * 2; // y = x + 1;
+            //y = x++ * 2; // y = x + 1;
+            //Console.WriteLine(y);
+            //Console.WriteLine(x);
+            //// += -= *= /=
+            
+
+
+             // LOGIKA
+            Console.WriteLine("Podaj wiek: ");
+            int wiek = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj ile masz pieniendzy");
+            double pieniadze = double.Parse(Console.ReadLine());
+
+            //// and -> && or -> || not ->!
+            ////bool czyMozeKupic = wiek >= 18 && pieniadze >=3.5;
+            bool czyMozeKupic = (wiek >= 18) && pieniadze >= 3.5;
+            Console.WriteLine(czyMozeKupic);
+            Console.WriteLine("Negacja");
+            Console.WriteLine(!czyMozeKupic);
+
+            // Operacje na stringach 
+            string imie;
+            //Console.WriteLine("Podaj imie: ");
+            imie = Console.ReadLine();
+            char znak = 'A';
+            char liczba = (char)122;
+            //Console.WriteLine("10" + 10);
+            Console.WriteLine(znak + 'B');
+            Console.WriteLine(liczba);
+            Console.ReadLine();
+
+            Console.WriteLine("Znak: "  + znak + "Liczba: " + liczba);
+            Console.WriteLine("Znak: {0} Liczba {1}", znak, liczba);
+        }
+    }
+}
